da57deec98f69af480af759ceec95d0e7ea34193
Triolla Dev Team <triolladev@Triollas-MacBook-Pro.local>
2025-09-07 22:30:11 +0300

    removed node

--- vcai-be/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import initPromptRoute from "@/routes/init-prompt.route";
-
-const router = Router();
-
-router.use("/init-prompt", initPromptRoute);
-
-export default router;
--- vcai-be/src/routes/init-prompt.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getInitPromptController } from "@/controllers/init-prompt.controller";
-
-const router = Router();
-
-router.get("/", getInitPromptController);
-
-export default router;
